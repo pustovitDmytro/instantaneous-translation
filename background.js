d9188426b7ca825b7df64c5ffbbfffa0e50fbd42
dmytroPustovit <pustic@gmail.com>
2017-03-17 00:05:49 +0200

    problems with object

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
 var working = false;
 
 chrome.runtime.onMessage.addListener(function(mes){
-	text = mes.text;
-
+	var text = mes.text;
+	var obj = {title: "Item1", message: "668686"};
+	obj.message = text;
+	var opt = { 
+		type: "list",
+		iconUrl: "3.png",
+		title: "translator",
+		message: "text",
+		items: [ {title: "Item1", message: "This is item 1."},
+		obj
+		]
+	}
+	chrome.notifications.create(opt);
 });
 
 chrome.browserAction.onClicked.addListener(function(){
