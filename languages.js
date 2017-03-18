3e1f6ed03cddf1bb6ef612805bf552861e69923f
dmytroPustovit <pustic@gmail.com>
2017-03-19 00:29:46 +0200

    available languages

---
+++ languages.js
@@ -0,0 +1,12 @@
+languages = [
+	{ abbr: "de", full: "German" },
+	{ abbr: "en", full: "English" },
+	{ abbr: "es", full: "Spanish" },
+	{ abbr: "it", full: "Italian" },
+	{ abbr: "ja", full: "Japanese" },
+	{ abbr: "pl", full: "Polish" },
+	{ abbr: "ru", full: "Russian" },
+	{ abbr: "tr", full: "Turkish"},
+	{ abbr: "uk", full: "Ukrainian" },
+	{ abbr: "zh", full: "Chinese" },
+	]
